58930bfbf5f97892ab28d27114f39ad3e03a75f6
STLnick <nickrray@gmail.com>
2023-02-11 18:13:35 -0600

    Change color usage and link up tester to dummy message in UI

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,15 @@
   import AppBar from './lib/AppBar.svelte';
   import Footer from './lib/Footer.svelte';
   import Chart from 'chart.js/auto';
+  import { _main } from './lib/index';
+
+  let message = '(test not ran yet)';
+
+  function handleClick() {
+    _main();
+
+    message = 'Ran the test!';
+  }
 
   window.requestAnimationFrame(() => {
     const ctx = document.getElementById('chart-canvas') as HTMLCanvasElement;
@@ -30,6 +39,15 @@
 
 <AppBar/>
 <main class="text-center p-4 mx-0">
+  <div class="h-24 flex items-center justify-center">
+    <button
+      class="bg-svelte-500 hover:bg-svelte-400 text-white font-bold py-2 px-4 border-b-4 border-svelte-700 hover:border-svelte-500 rounded"
+      on:click={handleClick}
+    >
+      Run Test
+    </button>
+    <div class="bg-slate-300">{message}</div>
+  </div>
   <div class="w-[800px] mx-auto">
     <canvas id="chart-canvas"></canvas>
   </div>
--- src/lib/AppBar.svelte
+++ src/lib/AppBar.svelte
@@ -2,9 +2,9 @@
   import jsLogo from '../assets/js-logo.svg';
 </script>
 
-<nav class="flex items-center justify-between flex-wrap bg-svelte p-6">
+<nav class="flex items-center justify-between flex-wrap bg-svelte-500 p-6">
   <div class="flex items-center flex-shrink-0 text-white">
     <img class="w-6 h-6" src="{jsLogo}" alt="JavaScript logo"/>
-    <span class="pl-1">Cloning Comparison</span>
+    <span class="pl-1">Clone Battles</span>
   </div>
 </nav>
