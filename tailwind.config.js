0add810dd60224b4634336ff2d680606538b3a19
STLnick <nickrray@gmail.com>
2023-02-11 18:11:31 -0600

    Establish color range for Svelte colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 module.exports = {
   mode: "jit",
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-  ],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
       colors: {
-        'svelte': '#FF3E00',
+        svelte: {
+          100: "#ffc5b3",
+          200: "#ff9f80",
+          300: "#ff8b66",
+          400: "#ff6533",
+          500: "#FF3E00",
+          600: "#cc3200",
+          700: "#992500",
+          800: "#801f00",
+          900: "#4c1300",
+        },
       },
-      fontFamily: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif']
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Open Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
     },
   },
   plugins: [],
