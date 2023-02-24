c529a9e9594b710e9049967a2d2b3f25843ad497
STLnick <nickrray@gmail.com>
2023-02-24 16:57:31 -0600

    Format run values and implement three column layout

--- src/lib/ResultsDisplay.svelte
+++ src/lib/ResultsDisplay.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import type { TestResults } from '$utils/interfaces';
 
-    export let results: TestResults;
-    $: arrayResults = Object.keys(results).map(key => ({ ...results[key], key}));
+  export let results: TestResults;
+  $: arrayResults = Object.keys(results).map(key => ({ ...results[key], key}));
 </script>
 
-<div class="w-[800px] mx-auto mt-10 p-4 bg-gray-200">
+<div class="w-[800px] flex mx-auto mt-10 p-4 bg-gray-200">
     {#each arrayResults as result}
-      <h3 class="text-lg font-bold">{result.key}</h3>
-      {#each result.runs as run}
-        <div>
-            {run}
-        </div>
-      {/each}
+      <div class="w-[33.3%] p-1">
+        <h3 class="text-lg font-bold">{result.key}</h3>
+        {#each result.runs as run}
+          <div>
+              {run.toFixed(2)}
+          </div>
+        {/each}
+      </div>
     {/each}
 </div>
